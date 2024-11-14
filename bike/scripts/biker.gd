c1fd27e5f49a921a2aebd308336c447c8e8d0962
gostok <stiv4ik12012@gmail.com>
2024-11-14 17:13:19 +0300

    update scale all sprites in Biker

--- bike/scripts/biker.gd
+++ bike/scripts/biker.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
+signal died
+
 enum State {
 	IDLE,
 	MOVING_LEFT,
-	MOVING_RIGHT
+	MOVING_RIGHT,
+	DEATH
 }
 
 var current_state = State.IDLE
@@ -13,16 +16,19 @@ var freeze_time = 0.1  # Время заморозки в секундах
 var freeze_timer = 0.0  # Таймер для отслеживания времени заморозки
 
 @onready var movement = preload("res://bike/scripts/playerMovement.gd").new()
-@onready var wheel_back = $WheelHolder_Back/wheel
+@onready var wheel_back = $wheels/WheelHolder_Back/wheel_back
 
 var wheels = []
 var deceleration = 200
-var speed = 1000
-var max_speed = 18
+var speed = 600
+var max_speed = 30
+
+var is_alive = true
 
 
 func _ready() -> void:
 	add_child(movement)
+
 
 func _physics_process(delta: float) -> void:
 	if is_frozen:
@@ -36,7 +42,7 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("jump"):
 		if wheel_back.angular_velocity < max_speed:
-			wheel_back.apply_torque_impulse(speed * delta * 60)
+			wheel_back.apply_torque_impulse(speed * delta * 20)
 
 
 func handle_input(delta: float):
@@ -59,11 +65,14 @@ func handle_input(delta: float):
 
 	if !is_left_pressed and !is_right_pressed:
 		freeze_state()
+	
+	
 
 
 func freeze_state():
 	is_frozen = true
 	freeze_timer = freeze_time
+
 
 func update_movement(delta: float):
 	match current_state:
@@ -71,7 +80,33 @@ func update_movement(delta: float):
 			pass
 		State.MOVING_RIGHT:
 			movement.move_rigth(delta)
-			apply_torque_impulse(200 * delta * 60)
+			apply_torque_impulse(20 * delta * 30)
 		State.MOVING_LEFT:
 			movement.move_left(delta)
-			apply_torque_impulse(-200 * delta * 60)
+			apply_torque_impulse(-10 * delta * 30)
+
+
+
+
+
+func death_player():
+	is_alive = false
+	emit_signal("died")
+	queue_free()
+	get_tree().change_scene_to_file("res://all_scenes/levels/level_1/level_1_0.tscn")
+
+
+
+func _on_head_death_body_entered(body) -> void:
+	if body is StaticBody2D:
+		death_player()
+
+
+func _on_body_death_body_entered(body) -> void:
+	if body is StaticBody2D:
+		death_player()
+
+
+func _on_hand_death_body_entered(body) -> void:
+	if body is StaticBody2D:
+		death_player()
--- bike/scripts/playerMovement.gd
+++ bike/scripts/playerMovement.gd
@@ -2,12 +2,12 @@ extends Node
 
 var offset_x = 0
 var offset_y = 0
-var max_offset_x = 5
-var max_offset_y = 1
+var max_offset_x = 1
+var max_offset_y = 0.2
 var move_speed = 100
 
 @onready var head_player = $"../player/skelet_player/pins/HeadHolder/head"
-@onready var body_player = $"../player/skelet_player/pins/Body_playerHolder/body_player"
+@onready var body_player = $"../player/skelet_player/pins/Body_playerHolder/body_b"
 @onready var hand_player = $"../player/skelet_player/pins/HandHolder/hand"
 @onready var hip_player = $"../player/skelet_player/pins/HipHolder/hip"
 @onready var shin_player = $"../player/skelet_player/pins/ShinHolder/shin"
@@ -38,23 +38,23 @@ func move_left(delta: float) -> void:
 		shin_player.position.y -= move_speed * delta
 		shin_player.rotation = deg_to_rad(-70)
 	else:
-		head_player.position.x = -7
+		head_player.position.x = -1.4
 		head_player.rotation = deg_to_rad(-15)
 		
-		body_player.position.x = -10
-		body_player.position.y = -3
+		body_player.position.x = -2
+		body_player.position.y = -0.6
 		body_player.rotation = deg_to_rad(22)
 		
-		hand_player.position.x = -5
+		hand_player.position.x = -1
 		hand_player.rotation = deg_to_rad(-20)
 		
-		hip_player.position.x = -12
-		hip_player.position.y = -6
+		hip_player.position.x = -2.4
+		hip_player.position.y = -1.2
 		hip_player.rotation = deg_to_rad(-12)
 		
-		shin_player.position.x = -6
-		shin_player.position.y = -6
-		shin_player.rotation = deg_to_rad(-70)
+		shin_player.position.x = -1
+		shin_player.position.y = -1.2
+		shin_player.rotation = deg_to_rad(-50)
 
 func move_rigth(delta: float) -> void:
 
@@ -63,7 +63,7 @@ func move_rigth(delta: float) -> void:
 		offset_y += move_speed * delta
 		
 		head_player.position.x = offset_x
-		head_player.position.y = 4
+		head_player.position.y = 0.8
 		head_player.rotation = deg_to_rad(15)
 		
 		body_player.position.x = offset_x
@@ -78,18 +78,18 @@ func move_rigth(delta: float) -> void:
 		shin_player.position.x -= move_speed * delta
 		shin_player.rotation = deg_to_rad(-15)
 	else:
-		head_player.position.x = 13
-		head_player.position.y = 4
+		head_player.position.x = 2.6
+		head_player.position.y = 0.8
 		head_player.rotation = deg_to_rad(15)
 		
-		body_player.position.x = 10
+		body_player.position.x = 2
 		body_player.rotation = deg_to_rad(15)
 		
-		hand_player.position.x = 3
+		hand_player.position.x = 0.6
 		hand_player.rotation = deg_to_rad(-10)
 		
-		hip_player.position.x = 3
+		hip_player.position.x = 0.6
 		hip_player.rotation = deg_to_rad(40)
 		
-		shin_player.position.x = -5
+		shin_player.position.x = -0.5
 		shin_player.rotation = deg_to_rad(-15)
