7225ddc9c9f17f7333eef2792091c66d05aa207b
gostok <stiv4ik12012@gmail.com>
2024-11-23 21:41:44 +0300

    biker gd update. create test_scene.gd

---
+++ all_scenes/test_scene/test_scene.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+@onready var line2d = $StaticBody2D/Line2D  # Ваш узел Line2D
+@onready var upper_line2d = $UpperLine2D  # Новый узел Line2D для верхней трассы
+@onready var collisionP = $StaticBody2D/collision
+@onready var statbod = $StaticBody2D
+
+var offset = 10.0  # Смещение для создания верхней линии
+var collision_offset = 2.5  # Смещение для коллизии
+var ground_y = 720  # Y-координата для нижней части коллизии
+var right_x = 1280  # X-координата для правой части коллизии
+
+func _ready():
+	duplicate_line()
+	duplicate_collision()
+	queue_redraw()
+
+func _draw():
+	var lower_points = line2d.points
+	var upper_points = upper_line2d.points
+	
+	# Проверяем, есть ли точки в обеих линиях
+	if lower_points.size() > 0 and upper_points.size() > 0:
+		# Закрашиваем пространство между линиями
+		var fill_color = Color(229 / 255.0, 21 / 255.0, 191 / 255.0, 1.0)  # Альфа-канал 
+		var polygon_points = get_polygon_between_lines(lower_points, upper_points)
+		draw_polygon(polygon_points, [fill_color])  # Передаем массив точек и массив цветов
+		draw_connecting_lines(lower_points, upper_points)  # Рисуем соединительные линии
+
+func duplicate_line():
+	var points = line2d.points  # Получаем точки из нижней линии
+	upper_line2d.clear_points()  # Очищаем предыдущие точки верхней линии
+	
+	for point in points:
+		upper_line2d.add_point(point + Vector2(0, -offset))  # Смещаем точку вверх
+
+	queue_redraw()
+
+func duplicate_collision():
+	var new_collision = CollisionPolygon2D.new()
+	var line_points = line2d.points
+	var new_polygon = []
+	
+	for point in line_points:
+		var new_point = point + Vector2(0, -collision_offset)
+		new_polygon.append(new_point)
+	
+	new_polygon.append(Vector2(right_x, ground_y))
+	new_polygon.append(Vector2(-line_points[line_points.size() - 1].x, ground_y))
+	
+	# Замыкаем полигон
+	if new_polygon.size() > 0:
+		new_polygon.append(new_polygon[0])  # Замыкаем полигон
+
+	new_collision.polygon = new_polygon
+	statbod.add_child(new_collision)
+	new_collision.set_deferred("disabled", false)
+
+func get_polygon_between_lines(lower_points: Array, upper_points: Array) -> Array:
+	var polygon = []
+	
+	for point in lower_points:
+		polygon.append(point)
+	
+	for i in range(upper_points.size() - 1, -1, -1):
+		polygon.append(upper_points[i])
+	
+	# Замыкаем полигон
+	if polygon.size() > 0 and polygon[0] != polygon[polygon.size() - 1]:
+		polygon.append(polygon[0])  # Замыкаем полигон, добавляя первую точку в конец
+
+	return polygon
+
+func draw_connecting_lines(lower_points: Array, upper_points: Array):
+	for i in range(lower_points.size()):
+		if i < upper_points.size():  # Проверка на соответствие размеров
+			var lower_point = lower_points[i]
+			var upper_point = upper_points[i]
+			var color_con_lines = Color(183/255.0, 5/255.0, 152/255.0)
+			draw_line(lower_point, upper_point, color_con_lines, 1.0)  # Цвет соединительных линий
--- bike/scripts/biker.gd
+++ bike/scripts/biker.gd
@@ -11,43 +11,42 @@ enum State {
 
 var current_state = State.IDLE
 
-var is_frozen = false
-var freeze_time = 0.1  # Время заморозки в секундах
-var freeze_timer = 0.0  # Таймер для отслеживания времени заморозки
 
 @onready var movement = preload("res://bike/scripts/playerMovement.gd").new()
 @onready var wheel_back = $wheels/WheelHolder_Back/wheel_back
+@onready var wheel_area_back = $wheels/WheelHolder_Back/wheel_back/Area2D_back
+
 
 var wheels = []
-var deceleration = 200
-var speed = 600
-var max_speed = 30
+var deceleration = 5000
+var speed = 3000
+var max_speed = 16
 
 var is_alive = true
-
+var is_on_ground_back = false  # Переменная для отслеживания состояния заднего колеса
+var current_speed = 0.0  # Текущая скорость
 
 func _ready() -> void:
 	add_child(movement)
 
-
 func _physics_process(delta: float) -> void:
-	if is_frozen:
-		freeze_timer -= delta
-		if freeze_timer <= 0:
-			is_frozen = false
-			current_state = State.IDLE
-	else:
-		handle_input(delta)
-		update_movement(delta)
 
-	if Input.is_action_pressed("jump"):
+	handle_input(delta)
+	update_movement(delta)
+
+	if Input.is_action_pressed("run"):
 		if wheel_back.angular_velocity < max_speed:
-			wheel_back.apply_torque_impulse(speed * delta * 20)
+			wheel_back.apply_torque_impulse(speed * delta * 60)
+	if Input.is_action_just_pressed("run") or is_on_ground_back==false:
+		wheel_back.angular_velocity = max(0, current_speed - deceleration * delta * 60)
+	if Input.is_action_pressed("stop"):
+		wheel_back.angular_velocity = 0
 
+	if Input.is_action_pressed("reset"):
+		get_tree().change_scene_to_file("res://all_scenes/test_scene/test_scene.tscn")
 
 func handle_input(delta: float):
-	if is_frozen:
-		return
+
 
 	var is_right_pressed = Input.is_action_pressed("right")
 	var is_left_pressed = Input.is_action_pressed("left")
@@ -55,58 +54,68 @@ func handle_input(delta: float):
 	if is_right_pressed and !is_left_pressed:
 		if current_state != State.MOVING_RIGHT:
 			current_state = State.MOVING_RIGHT
-
 	elif is_left_pressed and !is_right_pressed:
 		if current_state != State.MOVING_LEFT:
 			current_state = State.MOVING_LEFT
-
 	else:
 		current_state = State.IDLE
 
-	if !is_left_pressed and !is_right_pressed:
-		freeze_state()
-	
-	
 
-
-func freeze_state():
-	is_frozen = true
-	freeze_timer = freeze_time
 
 
 func update_movement(delta: float):
+	var torque = 0
+	var acceleration = 0
+
 	match current_state:
 		State.IDLE:
 			pass
 		State.MOVING_RIGHT:
 			movement.move_rigth(delta)
-			apply_torque_impulse(20 * delta * 30)
+			torque = 40
+			acceleration = speed * delta * 60
 		State.MOVING_LEFT:
 			movement.move_left(delta)
-			apply_torque_impulse(-10 * delta * 30)
+			torque = -40
+			acceleration = speed * delta * 60
+		State.DEATH:
+			death_player()
 
+	# Применяем крутящий момент
+	apply_torque_impulse(torque * delta * 30)
 
+	# Применяем силу для движения
+	apply_impulse(Vector2(0, 0), Vector2(acceleration * delta, 0))
 
-
+	# Применяем текущую скорость
+	apply_impulse(Vector2(0, 0), Vector2(current_speed * delta, 0))
 
 func death_player():
 	is_alive = false
 	emit_signal("died")
-	queue_free()
-	get_tree().change_scene_to_file("res://all_scenes/levels/level_1/level_1_0.tscn")
+	call_deferred("queue_free")
+	call_deferred("_change_scene")
 
-
+func _change_scene():
+	get_tree().change_scene_to_file("res://all_scenes/test_scene/test_scene.tscn")
 
 func _on_head_death_body_entered(body) -> void:
 	if body is StaticBody2D:
-		death_player()
-
+		current_state = State.DEATH
 
 func _on_body_death_body_entered(body) -> void:
 	if body is StaticBody2D:
-		death_player()
-
+		current_state = State.DEATH
 
 func _on_hand_death_body_entered(body) -> void:
 	if body is StaticBody2D:
-		death_player()
+		current_state = State.DEATH
+
+func _on_area_2d_back_body_entered(body: Node2D) -> void:
+	if body is StaticBody2D:
+		is_on_ground_back = true
+
+
+func _on_area_2d_back_body_exited(body: Node2D) -> void:
+	if body is StaticBody2D:
+		is_on_ground_back = false
