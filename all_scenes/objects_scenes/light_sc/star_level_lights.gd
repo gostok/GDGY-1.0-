b648bcbdbcbec80f747ba0029d892e252d1f6c60
gostok <stiv4ik12012@gmail.com>
2025-02-10 23:29:09 +0300

    level_vice

--- all_scenes/objects_scenes/light_sc/star_level_lights.gd
+++ all_scenes/objects_scenes/light_sc/star_level_lights.gd
@@ -1,11 +1,14 @@
 extends PointLight2D
 
+
 @onready var timer: Timer = $Timer
 
 
+
+
 func _on_timer_timeout() -> void:
-	var rng = randf_range(0.8, 1.5)
+	var rng = randf_range(0.6, 1.2)
 	var tween = get_tree().create_tween()
 	tween.parallel().tween_property(self, "texture_scale", rng, timer.wait_time)
 	tween.parallel().tween_property(self, "energy", rng, timer.wait_time)
-	timer.wait_time = randf_range(0.2, 0.6)
+	timer.wait_time = randf_range(0.4, 0.8)
