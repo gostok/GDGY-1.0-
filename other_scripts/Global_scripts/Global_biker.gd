bd92689ec2854299f4dff47b6c7cb18dcd9ce471
gostok <stiv4ik12012@gmail.com>
2025-02-23 11:11:55 +0300

    updated 0.42 | dialogue update

--- other_scripts/Global_scripts/Global_biker.gd
+++ other_scripts/Global_scripts/Global_biker.gd
@@ -5,6 +5,8 @@ var start_vice_biker: bool = false
 # переменная для проверки, первое ли прохождение карты 'Abyss'
 var start_abyss_biker: bool = false
 var dark_guide: bool = false
+# переменная для проверки, первое ли прохождение карты 'Neon_Streets'
+var start_ns_biker: bool = false
 
 
 # Переменные для разных концовок сюжета игры
